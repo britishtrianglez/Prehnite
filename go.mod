c6d53d05735d34f15cbd08f22ca168fcb5dc9c1d
britishtrianglez <jackafoster1@gmail.com>
2023-06-19 10:29:51 +0100

    Init Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/britishtrianglez/Prehnite
+
+go 1.20
---
+++ scr/nodes/variables/types.go
@@ -0,0 +1,11 @@
+package variables
+
+func String() string {
+	return "String"
+}
+func Bool() string {
+	return "Bool"
+}
+func Int() string {
+	return "Int"
+}
